7155363a3feb91d000003bbc9573cdd238a2d74c
Daniel Diaz <danielstiven35@gmail.com>
2026-01-28 11:44:48 -0500

    Configuración para que el componente reaccionea los cambios del idioma y color

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UiStateService } from './services/ui-state.service';
+import { TranslatePipe } from './pipes/translate.pipe';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, TranslatePipe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('daniel-web');
+  public ui = inject(UiStateService);
+  protected readonly title = this.ui.theme() === 'dark' ? 'Daniel Web Dark' : 'Daniel Web';
 }
